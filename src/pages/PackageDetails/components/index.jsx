bc3670369a3fa78423d664cd5628ebf6d3f7b967
habib33-3 <habib.swi16@gmail.com>
2024-05-12 11:17:16 +0600

    cleaned for new project setup

--- src/pages/PackageDetails/components/index.jsx
+++
@@ -1,5 +0,0 @@
-import Details from "./Details/Details";
-import AvailableGuides from "./AvailableGuides/AvailableGuides";
-import BookingForm from "./BookingForm/BookingForm";
-
-export { Details, AvailableGuides, BookingForm };
