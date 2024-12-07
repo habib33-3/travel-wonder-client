0816d5d34e3ab8f170ca1a95ca4025f26b901e30
habib33-3 <habib.rahman0330@gmail.com>
2024-12-07 22:35:57 +0600

    react router implemented

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
 import { StrictMode } from "react";
 
 import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router";
 
-import App from "./App.tsx";
 import "./index.css";
+import router from "./router/Router.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
